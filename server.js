8d6ac7a9c0306f4e6180c3b409ae59bdb243d781
Steve Duong <steve@Plan.D>
2019-03-13 12:24:29 -0700

    configures settings for deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -7,18 +8,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8008;
 
-app.use(compression());
-app.use(morgan('dev'));
+// app.use(compression());
+// app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/photos', proxy({
-  target: 'http://ec2-18-206-121-61.compute-1.amazonaws.com'
+  target: 'ec2-18-222-149-148.us-east-2.compute.amazonaws.com'
 }));
-app.use('/api/reserve', proxy({
-  target: 'http://ec2-18-191-229-0.us-east-2.compute.amazonaws.com/'
-}));
-app.use('/menu', proxy({
-  target: 'http://ec2-3-17-28-103.us-east-2.compute.amazonaws.com'
-}));
+// app.use('/api/reserve', proxy({
+//   target: 'http://ec2-18-191-229-0.us-east-2.compute.amazonaws.com/'
+// }));
+// app.use('/menu', proxy({
+//   target: 'http://ec2-3-17-28-103.us-east-2.compute.amazonaws.com'
+// }));
 // app.use('/overview', proxy({
 //   target: 'http://ec2-18-191-13-163.us-east-2.compute.amazonaws.com/'
 // }));
