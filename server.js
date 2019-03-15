31e62542db6169f5dca9af7bbf36df2c258ae15c
Steve Duong <steve@Plan.D>
2019-03-15 16:31:45 -0700

    changes links to loadbalance links

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 8008;
 // app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/photos', proxy({
-  target: 'http://ec2-18-222-149-148.us-east-2.compute.amazonaws.com/'
+  target: 'http://ec2-18-191-228-123.us-east-2.compute.amazonaws.com/'
 }));
 app.use('/api/reserve', proxy({
   target: 'http://ec2-18-222-239-157.us-east-2.compute.amazonaws.com/'
@@ -21,7 +21,7 @@ app.use('/api/menu', proxy({
   target: 'http://ec2-18-223-135-231.us-east-2.compute.amazonaws.com/'
 }));
 app.use('/overview', proxy({
-  target: 'http://ec2-3-92-162-226.compute-1.amazonaws.com/'
+  target: 'http://ec2-35-171-26-238.compute-1.amazonaws.com/'
 }));
 // app.use('/overview', proxy({
 //   target: ''
